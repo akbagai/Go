061b140ae9e918754316cf07b7f2d0474fb7efbe
Paul Crickard <paulcrickard@gmail.com>
2015-07-06 10:39:48 -0600

    Update ESRI-REST-test.go
    added comment.

--- ESRI-REST-test.go
+++ ESRI-REST-test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main(){
-
+ //Pass query parameters in URL. MUST HAVE -----> f=json
 d :=esrirest.Query("http://coagisweb.cabq.gov/arcgis/rest/services/public/PublicArt/MapServer/0/query?where=1=1&outFields=*&f=json")
 fmt.Println(d[0].Attributes["TITLE"])
       /* Iterate through the features and print TITLE
