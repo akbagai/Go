4ff3a3674b8d4d43f08074302ba4d5f951fa7868
Paul Crickard <paulcrickard@gmail.com>
2015-07-07 13:42:55 -0600

    Create Handlers.go

---
+++ Server/Handlers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type query struct {
+	Features []features
+}
+
+type features struct {
+	Properties map[string]interface{}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintln(w, "Welcome!")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	content, err := ioutil.ReadFile("C:/Users/e31757/Desktop/GoCode/src/ReadDirectory/files/art.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(content)
+	var data query
+	json.Unmarshal(content, &data)
+	if err := json.NewEncoder(w).Encode(data.Features); err != nil {
+		panic(err)
+	}
+}
