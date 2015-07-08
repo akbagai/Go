277e75b4dff0b280ab8ceb5c3ddfc41ce75e3e17
Paul Crickard <paulcrickard@gmail.com>
2015-07-08 07:26:27 -0600

    New Route
    Added a FilterQuery Route

--- Server/Handlers.go
+++ Server/Handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type query struct {
@@ -18,7 +20,7 @@ type features struct {
 func Index(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintln(w, "Welcome!")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	content, err := ioutil.ReadFile("C:/Users/Me/Desktop/GoCode/src/Server/files/art.json")
+	content, err := ioutil.ReadFile("C:/Users/me/Desktop/GoCode/src/server/files/art.json")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -28,4 +30,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewEncoder(w).Encode(data.Features); err != nil {
 		panic(err)
 	}
+}
+
+func FilterQuery(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	content, err := ioutil.ReadFile("C:/Users/me/Desktop/GoCode/src/server/files/art.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(content)
+	var data query
+	json.Unmarshal(content, &data)
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value := vars["value"]
+	for i := 0; i < len(data.Features); i++ {
+		if data.Features[i].Properties[key] == value {
+			if err := json.NewEncoder(w).Encode(data.Features[i]); err != nil {
+				panic(err)
+			}
+		} else {
+		}
+	}
+
 }
