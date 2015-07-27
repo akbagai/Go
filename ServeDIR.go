f5c4f9041bada0bbe1725c377ae1fcb1180b03b1
Paul Crickard <paulcrickard@gmail.com>
2015-07-27 12:13:39 -0600

    Update ServeDIR.go

--- ServeDIR.go
+++ ServeDIR.go
@@ -6,3 +6,25 @@ func main() {
 	dir := http.FileServer(http.Dir("assets"))
 	http.ListenAndServe(":9000", dir)
 }
+
+//serve directory binary is in:
+/*
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	serveThisDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	dir := http.FileServer(http.Dir(serveThisDir))
+	http.ListenAndServe(":9000", dir)
+}
+
+*/
